b7d536707195e0ca609040b4a805470a9b667a97
sast-ctrl <81892179+sast-ctrl@users.noreply.github.com>
2022-02-01 16:52:40 -0600

    Added signup functionality

--- src/components/UserFormSignup.js
+++ src/components/UserFormSignup.js
@@ -1,6 +1,83 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+import { API_URL } from '../App';
 
 const UserFormSignup = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+
+    const usernameChanged = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const passwordChanged = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const password2Changed = (event) => {
+        setPassword2(event.target.value);
+    };
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+
+        signup();
+    }
+
+    const addInvalidMsg = () => {
+        Array.from(document.querySelectorAll('.form-control')).forEach((el) => el.classList.add('is-invalid'));
+    };
+
+    const removeInvalidMsg = () => {
+        Array.from(document.querySelectorAll('.form-control')).forEach((el) => el.classList.remove('is-invalid'));
+    };
+
+    const signup = async () => {
+        removeInvalidMsg();
+
+        try {
+            const data = {
+                username,
+                password,
+                password2,
+            };
+
+            await axios.post(`${API_URL}/signup/`, data);
+            login();
+
+        } catch(error) {
+            addInvalidMsg();
+            console.error(`Error signup: ${error}`);
+        }
+
+    }
+
+    const login = async () => {
+        removeInvalidMsg();
+
+        try {
+            const data = {
+                username,
+                password,
+            };
+
+            const result = await axios.post(`${API_URL}/token/`, data);
+
+            const access = result.data.access;
+            const refresh = result.data.refresh;
+
+            localStorage.setItem('jwt-token', access);
+            localStorage.setItem('jwt-refresh', refresh);
+
+            window.location.href = '/';
+        } catch (error) {
+            addInvalidMsg();
+            console.error(`Error login: ${error}`);
+        }
+    }
+
     return (
         <>
             <hr className='mt-5' />
@@ -9,22 +86,22 @@ const UserFormSignup = () => {
                 <div className='col-12 mb-4'>
                     <h1 className='text-center mb-4'> Sign up </h1>
 
-                    <form onSubmit={''}>
+                    <form onSubmit={handleSubmit}>
                         <div className='form-group'>
                             <label htmlFor='inputUsername'>Username</label>
-                            <input onChange={''} type={'text'} className='form-control' id='inputUsername' placeholder='username' required />
+                            <input onChange={usernameChanged} type={'text'} className='form-control' id='inputUsername' placeholder='username' required />
                             <div className='invalid-feedback'> Invalid Credetentials </div>
                         </div>
 
                         <div className='form-group'>
                             <label htmlFor='inputPassword'>Password</label>
-                            <input onChange={''} type={'password'} className='form-control' id='inputPassword' placeholder='password' required />
+                            <input onChange={passwordChanged} type={'password'} className='form-control' id='inputPassword' placeholder='password' required />
                             <div className='invalid-feedback'> Invalid Credetentials </div>
                         </div>
-                        
+
                         <div className='form-group'>
                             <label htmlFor='inputPassword'>Confirm your password</label>
-                            <input onChange={''} type={'password'} className='form-control' id='inputPassword' placeholder='password' required />
+                            <input onChange={password2Changed} type={'password'} className='form-control' id='inputPassword' placeholder='password' required />
                             <div className='invalid-feedback'> Invalid Credetentials </div>
                         </div>
 
