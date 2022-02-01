308737035c855572fe9e1e4cd121e56c63cccb62
sast-ctrl <81892179+sast-ctrl@users.noreply.github.com>
2022-01-31 22:47:48 -0600

    Rating functionality added

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
 
-const MovieCard = ({ title, releaseDate, genre, plot }) => {
+import React from 'react';
+import ReactStars from 'react-rating-stars-component';
+import { Link } from 'react-router-dom';
+
+const MovieCard = ({ title, releaseDate, genre, plot, avgRating, slug, withLink = false }) => {
   return <div>
     <div className="card">
       <div className="card-body">
-        <h5 className="card-title mb-3">{title} <span className="badge badge-primary ml-2">{genre}</span></h5>
+        <div className='d-flex flex-row align-items-center'>
+          <ReactStars count={5} size={24} isHalf={true} edit={false} value={avgRating} activeColor='#ffd700' />
+          <span className='ml-2'>{avgRating.toFixed(1)}</span>
+        </div>
         <h6 className="card-subtitle mb-2 text-muted">Release date: {releaseDate}</h6>
 
         <p className='card-text'>{plot}</p>
