d534af5a66bdefe19653861cbd3b5209a9c79e9b
sast-ctrl <81892179+sast-ctrl@users.noreply.github.com>
2022-02-01 10:58:02 -0600

    Movie Details page now displays the reviews

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { API_URL } from '../App';
+import dayjs from 'dayjs';
+import ReactStars from 'react-rating-stars-component';
 
 
 const MovieDetails = ({ slug }) => {
@@ -32,7 +34,24 @@ const MovieDetails = ({ slug }) => {
                         </div>
                         <h2>Reviews</h2>
 
-                        {movie.ratings && movie.ratings.length > 0}
+                        {movie.ratings && movie.ratings.length > 0 &&
+                            movie.ratings.map((rating) => {
+                                return (
+                                    <div className="jumbotron jumbotron-fluid" style={{ width: '100%' }}>
+                                        <div className="container">
+                                            <h6 className="display-5"> {rating.author.username} </h6>
+                                            <h7 className='card-subtitle mb-3 text-muted'>
+                                                {dayjs(rating.created_at).format('MMM DD, YYYY')}
+                                            </h7>
+                                            <div className='d-flex flex-row align-items-center'>
+                                                <ReactStars count={5} size={24} isHalf={true} edit={false} value={rating.rating.toFixed(1)} activeColor='#ffd700' />
+                                                <span className='ml-2'>{rating.rating.toFixed(1)}</span>
+                                            </div>
+                                            <p className="lead my-4">{rating.comment}</p>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                     </>
                 )
             }
